f3154d7ed5cb95cfde07c1ee2bc23fc775e3f22f
Rashad <rashad.suleymanov83@gmail.com>
2025-02-24 23:15:55 +0400

    Add about GetMapping

--- src/main/java/com/my_spring_boot_project/demo/controllers/MainController.java
+++ src/main/java/com/my_spring_boot_project/demo/controllers/MainController.java
@@ -1,8 +1,23 @@
 package com.my_spring_boot_project.demo.controllers;
 
-public class MainControllers {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
+public class MainController {
 
+  @GetMapping("/")
+  public String home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    model.addAttribute("name", name);
+    return "home";
+  }
 
+  @GetMapping("/about")
+  public String about(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    model.addAttribute("name", name);
+    return "home";
+  }
 
 }
